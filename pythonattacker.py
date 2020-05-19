c22067af7a9432bb9aa3bb7b8be44225c2702f65
Sides <shelside@f01898654dbb.ant.amazon.com>
2020-05-18 23:11:52 -0500

    made region dynamic

--- pythonattacker.py
+++ pythonattacker.py
@@ -2,7 +2,7 @@ import boto3, json, time, uuid, random, os
 some_random_number = str(random.randrange(100000))
 
 # add access key to seccrets manager
-secrets_manager = boto3.client('secretsmanager', region_name='us-east-2')
+secrets_manager = boto3.client('secretsmanager', region_name=os.environ['AWS_DEFAULT_REGION'])
 secret_name = 'some_super_secret-' + str(some_random_number)
 
 response = secrets_manager.create_secret(
